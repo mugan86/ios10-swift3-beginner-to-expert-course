628460b19a8301a2aa8e94799a85a3bc749acc15
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-30 09:26:14 +0100

    Start with Section 4 (Strings Playground

---
+++ SECTION 4 - Strings.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - Strings (More info in http://blog.udacity.com/2015/03/learn-swift-tutorial-fundamentals.html)
+
+var name: String = "Anartz Mugika Ledo"
+
+name.uppercased()
+
+name.lowercased()
+
+var idUser = ""
+
+idUser = "182734739293333"
+
+
+//Video: 32 - Exercise
+
+var favouriteFood:String = "Ensalada"
+
+var petName: String
+
+petName = "Gato"
+
+
+//#1: Declare multiple variables on a single line with commas.
+var one_value = true, two_value = false
+
+//#2: Access the emoji keyboard using ctrl + cmd + spacebar!
+
+var show_emoji:String = "😂😅"
+
+
