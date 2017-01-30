29834ea3bbf6ca4073660024359bf0be3ad5cdde
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-30 08:54:51 +0100

    Update Playground with last exercises and Quiz Q&A

--- iOS10_Swift3_Course_Section2_PlayGround.playground/Contents.swift
+++ iOS10_Swift3_Course_Section2_PlayGround.playground/Contents.swift
@@ -1,16 +1,39 @@
-var str = "Hello playground"
+//Start str with String type
+var str: String = "Hello playground"
 
-str
+print(str)
 
-var name = "Anartz"
+var name: String = "Anartz"
 
-var age = 31
+//Start int value to asign age
+var age: Int = 31
 
-var nickname = "AM"
+var nickname: String = "AM"
 
-var pi = 3.1416
 
+//Concatenate String with 'name' variable
 str = "Hello \(name)"
 
 
+var weather: String = "Soleado"
 
+weather = "Lluvioso"
+
+//Comments (one line)
+
+/*
+ More one line comment
+ */
+
+//CONSTANTS
+let pi: Float=3.1416
+
+let birthYear: String
+
+birthYear = "1986/01/10"
+
+//QUIZ 1 Section 2 - Variables y Constantes (PlayGround)
+
+//PAra declarar una variable usamos 'var', para una constante 'let'
+
+//Una variable se diferencia de una constante porque puede cambiar su valor
