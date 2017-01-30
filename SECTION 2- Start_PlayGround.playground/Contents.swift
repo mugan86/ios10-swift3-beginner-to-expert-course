b95b90ed03a7a79076f7ee4465fbca377439a7f8
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-30 09:26:32 +0100

    Rename with correct name

---
+++ SECTION 2- Start_PlayGround.playground/Contents.swift
@@ -0,0 +1,39 @@
+//Start str with String type
+var str: String = "Hello playground"
+
+print(str)
+
+var name: String = "Anartz"
+
+//Start int value to asign age
+var age: Int = 31
+
+var nickname: String = "AM"
+
+
+//Concatenate String with 'name' variable
+str = "Hello \(name)"
+
+
+var weather: String = "Soleado"
+
+weather = "Lluvioso"
+
+//Comments (one line)
+
+/*
+ More one line comment
+ */
+
+//CONSTANTS
+let pi: Float=3.1416
+
+let birthYear: String
+
+birthYear = "1986/01/10"
+
+//QUIZ 1 Section 2 - Variables y Constantes (PlayGround)
+
+//PAra declarar una variable usamos 'var', para una constante 'let'
+
+//Una variable se diferencia de una constante porque puede cambiar su valor
