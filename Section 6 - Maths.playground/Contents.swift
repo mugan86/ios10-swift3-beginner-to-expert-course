e9ec0e328b1065fbba96d8da41d15868be06ada4
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-30 12:21:08 +0100

    Start with aritmetical numbers and exercises

---
+++ Section 6 - Maths.playground/Contents.swift
@@ -0,0 +1,89 @@
+//: Playground - Aritmética elemental
+
+//Operaciones con enteros Section 6 - Lecture 54
+let x = 7
+let y = 5
+
+x + y
+x - y
+x+y
+x-y
+x / y
+x * y
+x % y
+
+3 * 10 + 5
+
+(3 * 10) + 5
+
+3 * (10 + 5)
+
+3 * ( (10 + 5) - 5 ) * 3
+
+let myAge = 31
+
+let myWifeAge = 28
+
+myAge - myWifeAge
+
+myAge + myWifeAge
+
+(myAge + myWifeAge) % 3
+
+(myAge + myWifeAge) * 4
+
+/**************************************************************
+ Ejercicios de aritmética
+ Section 6, Lecture 55
+ Crea dos variables entera, una con el valor del año de nacimiento de tu padre, y otro con el año de nacimiento de tu madre.
+ Suma dichos valores
+ Resta dichos valores
+ Multiplica el primero por 2 y súmale el segundo dividido por 2
+ Adivina si alguno de ellos es par haciendo el módulo del número por dos.
+ **************************************************************/
+
+//Crea dos variables entera, una con el valor del año de nacimiento de tu padre, y otro con el año de nacimiento de tu madre.
+
+var nacimientoPadre = 1960
+var nacimientoMadre = 1964
+
+//Suma dichos valores
+nacimientoMadre + nacimientoPadre
+
+//Resta dichos valores
+nacimientoMadre - nacimientoPadre
+
+//Multiplica el primero por 2 y súmale el segundo dividido por 2
+(nacimientoPadre * 2) + (nacimientoMadre / 2)
+
+nacimientoPadre % 2 //Mirar si es par (=0)
+nacimientoMadre % 2 //Mirar si es par (=0)
+
+
+
+/*********************************************************************************************
+Numeros decimales, floats y doubles / Section 6 - Lecture 56
+*********************************************************************************************/
+
+let weight_float: Float = 68.39393939390302092029309393020209029292922229229929292922
+let weight_double: Double = 68.39393939390302092029309393020209029292922229229929292922
+let weight_decimal: Float = 68.39393939390302092029309393020209029292922229229929292922
+
+let age: Float = 31
+let age_without_type = 31.0
+
+age / 3
+age_without_type / 3
+
+let pi = 3.12
+
+/**************************************************************
+ Ejercicios de aritmética
+ Section 6, Lecture 57
+ Busca en la wikipedia información sobre el número e.
+ Crea una variable de tipo float para guardar su valor.
+ Busca en la wikipedia información sobre el número de oro.
+ Crea una variable de tipo double para guardar su valor.
+ **************************************************************/
+
+let e:Float = 2.71828182845904523536
