ed2dd2766d0a2df760c6f320c265345fafdeedd2
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-30 11:16:04 +0100

    Start with Section 5 in  Section 5 - Start new project with Xcode

---
+++ XCode Projects/Section5HelloWorld/Section5HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Section5HelloWorld
+//
+//  Created by Anartz Mugika on 30/1/17.
+//  Copyright © 2017 Anartz Mugika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
