35abc6019198b404713efdf3b69f75c2d757a359
anartzmugika <mugan86@gmail.com>
2017-01-31 20:28:04 +0100

    Define first actions

--- XCode Projects/section_5-lecture-50/section_5-lecture-50/ViewController.swift
+++ XCode Projects/section_5-lecture-50/section_5-lecture-50/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var nameInputTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addStudentButton(_ sender: Any) {
+        
+        let getNameFromNameInputTextField = nameInputTextField.text!
+        
+        print(getNameFromNameInputTextField)
+
+    }
 
 }
 
