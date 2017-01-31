e3b4cb99a1bde0fc3b58017ab2c537fe88ea00fd
anartzmugika <mugan86@gmail.com>
2017-01-31 20:27:45 +0100

    Start Section 5 Lecture 50 exercise Main Layout first steps

---
+++ XCode Projects/section_5-lecture-50/section_5-lecture-50/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  section_5-lecture-50
+//
+//  Created by Anartz Mugika on 31/1/17.
+//  Copyright © 2017 Anartz Mugika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
