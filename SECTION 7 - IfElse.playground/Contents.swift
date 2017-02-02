bdc02b2a4d69970fc9ef1e89326784c5ddb72499
Anartz Muxika Ledo <mugan86@gmail.com>
2017-02-02 09:56:22 +0100

    Start with playground in section 7

---
+++ SECTION 7 - IfElse.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+let age : Int = 31
+
+let name : String = "Anartz"
+
+let isSunny : Bool = true
+
+//let isCloud : Bool = "12" Not posiblem initialize string!!!
+
+//let isCloud : Bool = 1  Not posiblem initialize int!!!
