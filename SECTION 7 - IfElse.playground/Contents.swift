fa1602763f52c5e0dbda47a13706ea2f7b114d69
Anartz Muxika Ledo <mugan86@gmail.com>
2017-02-02 10:13:30 +0100

    Section 7 Lecture 66 - Exercise

--- SECTION 7 - IfElse.playground/Contents.swift
+++ SECTION 7 - IfElse.playground/Contents.swift
@@ -4,8 +4,16 @@ let age : Int = 31
 
 let name : String = "Anartz"
 
-let isSunny : Bool = true
+var isSunny : Bool = true
 
 //let isCloud : Bool = "12" Not posiblem initialize string!!!
 
 //let isCloud : Bool = 1  Not posiblem initialize int!!!
+
+isSunny = false
+
+/*Ejercicio de booleanos
+Sección 7, Clase 66
+Crea una variable booleana isWoman, y asígnala a verdadero o falso según si eres o no una mujer.*/
+
+let isWoman: Bool = false
