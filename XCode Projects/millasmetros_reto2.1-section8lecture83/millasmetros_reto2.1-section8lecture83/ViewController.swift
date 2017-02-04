a3aa7e10a2f2d35ec3298948020de80d5b4cfd86
anartzmugika <mugan86@gmail.com>
2017-02-04 10:10:20 +0100

    Add new project, lecture 83 in section 8. Extend advanced version to basic example

---
+++ XCode Projects/millasmetros_reto2.1-section8lecture83/millasmetros_reto2.1-section8lecture83/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  millasmetros_reto2.1-section8lecture83
+//
+//  Created by Anartz Mugika on 4/2/17.
+//  Copyright © 2017 Anartz Mugika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
