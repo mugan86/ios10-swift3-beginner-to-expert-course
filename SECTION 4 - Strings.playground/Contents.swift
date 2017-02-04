b2866b797c5b0151a8272b1ea071a61667cd95dd
anartzmugika <mugan86@gmail.com>
2017-02-04 09:21:46 +0100

    Add loop to check strings and 'substring' example

--- SECTION 4 - Strings.playground/Contents.swift
+++ SECTION 4 - Strings.playground/Contents.swift
@@ -1,4 +1,5 @@
 //: Playground - Strings (More info in http://blog.udacity.com/2015/03/learn-swift-tutorial-fundamentals.html)
+//https://oleb.net/blog/2016/08/swift-3-strings/
 
 var name: String = "Anartz Mugika Ledo"
 
@@ -45,6 +46,24 @@ print(🏖)
 let s = "abcdef"
 
 let charsCount = s.characters.count
-// Advance 5 from the start
-let idx = s.index(s.startIndex, offsetBy: charsCount-1) // To shos only last position
+// Advance 5 from the start (only show one char)
+let idx = s.index(s.startIndex, offsetBy: charsCount-1) // To show only last position
+let idx_ = s.index(s.startIndex, offsetBy: 4) // To shos only last position
 s[idx] // → "f" (the Character, not the String)
+s[idx_]
+
+for index in 1...5  {
+    print("\(index) times 5 is \(index * 5)")
+}
+
+var i = 0;
+var check : Bool = false
+repeat {
+    print("\(i) times 5 is \(i * 5)")
+    i += 1
+    if i == 2
+    {
+        check = true
+    }
+    
+} while i < 5 && check == false
