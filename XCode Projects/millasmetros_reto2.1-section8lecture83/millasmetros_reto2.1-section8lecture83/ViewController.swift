9f8342fb83b61de236ef326526d949680202abc4
anartzmugika <mugan86@gmail.com>
2017-02-04 10:24:21 +0100

    Update advanced example

--- XCode Projects/millasmetros_reto2.1-section8lecture83/millasmetros_reto2.1-section8lecture83/ViewController.swift
+++ XCode Projects/millasmetros_reto2.1-section8lecture83/millasmetros_reto2.1-section8lecture83/ViewController.swift
@@ -6,6 +6,17 @@
 //  Copyright © 2017 Anartz Mugika. All rights reserved.
 //
 
+/*
+ Reto 2.1: Mejoremos la app con los conocimientos que tenemos
+ Sección 8, Clase 83
+ Ahora que tenemos la app lista, es hora de mejorarla un poquito:
+ 
+ Añade un segundo segmented control para elegir unidad de partida y unidad de llegada
+ Modifica el código para convertir según las unidades de partida y llegada seleccionadas
+ Añade un tercer ítem a cada segmented control mostrando la unidad yardas
+ Modifica el código para convertir a yardas si el usuario las selecciona
+ Refactoriza el código para hacer las transformaciones correctamente (prueba a crear una función llamada converFrom(fromUnit:, toUnit) para no repetir código innecesariamente.*/
+
 import UIKit
 
 class ViewController: UIViewController {
