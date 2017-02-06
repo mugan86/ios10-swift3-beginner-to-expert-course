7353363a452188d75841bc9516976dff56a52ef8
Anartz Muxika Ledo <mugan86@gmail.com>
2017-02-06 08:14:50 +0100

    Add layout elements code to communicate in app ViewController

--- XCode Projects/conversor divisas/conversor divisas/ViewController.swift
+++ XCode Projects/conversor divisas/conversor divisas/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var inputCoinTextField: UITextField!
+    
+    @IBOutlet var coinSelectionSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet var selectionCoinLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet var coinConvertButton: UIButton!
 
 }
 
