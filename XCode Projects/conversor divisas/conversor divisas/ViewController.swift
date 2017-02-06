ee9bd541ccd549b0385efec398d6fef6ca414085
Anartz Muxika Ledo <mugan86@gmail.com>
2017-02-06 08:23:30 +0100

    Define ViewController with Button action to convert

--- XCode Projects/conversor divisas/conversor divisas/ViewController.swift
+++ XCode Projects/conversor divisas/conversor divisas/ViewController.swift
@@ -4,12 +4,20 @@
 //
 //  Created by Anartz Mugika on 6/2/17.
 //  Copyright © 2017 Anartz Mugika. All rights reserved.
+//  Elige varias divisas (Euro, Dólar, Peso, Libras, Reales...) y elabora un conversor de divisas con los elementos que has ido conociendo a lo largo de las clases hasta el momento. Consulta el estado actual de la relación entre divisas (1 € = 0.895 $, por ejemplo)
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    //1 euro in other divises
+    //Info extract from https://conversormoneda.com/
+    let bitCoinUnit : Double = 0.0011
+    let dollarUnit : Double = 1.08
+    let libraUnit : Double = 0.86
+    let yenUnit : Double = 121.29
+    
     @IBOutlet var inputCoinTextField: UITextField!
     
     @IBOutlet var coinSelectionSegmentedControl: UISegmentedControl!
@@ -26,7 +34,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBOutlet var coinConvertButton: UIButton!
+    @IBAction func convertCoinInOthersButton(_ sender: Any) {
+        //Check if input is empty
+        //Check if last char in textfield is "point"
+    }
+    
+   
 
 }
 
