58c9c3c3abdea3689f8ef074045bb5e26c4f3473
Anartz Muxika Ledo <mugan86@gmail.com>
2017-02-06 08:27:17 +0100

    Check situations functions add

--- XCode Projects/conversor divisas/conversor divisas/ViewController.swift
+++ XCode Projects/conversor divisas/conversor divisas/ViewController.swift
@@ -28,6 +28,38 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //To return last position character to check points
+    func getInputDistanceTextFieldLastChar(_currentTextInInputTextField: String, _select_position: Int) -> Character
+    {
+        let charPos = _currentTextInInputTextField.index(_currentTextInInputTextField.startIndex, offsetBy: _select_position) // Select position
+        return _currentTextInInputTextField[charPos]
+    }
+    
+    //Clean last point in TextField (To remove last position 'point' char
+    func cleanPointInLastPosition(_charsCount: Int, _currentTextInInputDistanceTextField: String)
+    {
+        var newStringWithRemoveUnnecesaryPoints : String = ""
+        
+        //Check if point and string length = 2
+        
+        if (_currentTextInInputDistanceTextField.characters.count == 1)
+        {
+            focusInInput()
+            showAlertMessage(_title: "Conversión erronea", _message: "No puedes realizar la conversión únicamente introduciendo un punto. Añade un número correcto por favor", _type: 2)
+            return;
+        }
+        else
+        {
+            for index in 0..._charsCount - 2  {
+                
+                let char = getInputDistanceTextFieldLastChar(_currentTextInInputTextField: _currentTextInInputDistanceTextField, _select_position: index)
+                newStringWithRemoveUnnecesaryPoints += String(char)
+                inputCoinTextField.text = newStringWithRemoveUnnecesaryPoints
+            }
+        }
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -38,6 +70,39 @@ class ViewController: UIViewController {
         //Check if input is empty
         //Check if last char in textfield is "point"
     }
+    
+    func showAlertMessage(_title:String, _message:String, _type: Int)
+    {
+        //Configuration to alert Controller with title, message and use prefered style
+        let alertController: UIAlertController =
+            UIAlertController(title: _title,
+                              message: _message,
+                              preferredStyle: .alert)
+        
+        //Add action button to close alert Controller after show in app
+        //style: default = OK in blue default color / destructive (red color)
+        
+        let okAlertButton: UIAlertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        //Add action after configure to use in alertController
+        alertController.addAction(okAlertButton)
+        
+        if (_type == 1)
+        {
+            let cancelAlertButton: UIAlertAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+            
+            alertController.addAction(cancelAlertButton)
+        }
+        
+        //Show Alert controller with alertControllerUI elements and OK action
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func focusInInput()
+    {
+        inputCoinTextField.becomeFirstResponder()
+    }
+
     
    
 
