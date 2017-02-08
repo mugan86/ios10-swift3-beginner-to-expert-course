2c3e27c73e09595bb921b6231a319cd74d2ef7a4
Anartz Muxika Ledo <mugan86@gmail.com>
2017-02-08 11:32:24 +0100

    Update with Switch example

--- Section_10_Switch.playground/Contents.swift
+++ Section_10_Switch.playground/Contents.swift
@@ -1,5 +1,5 @@
 //Playground - Switch
-let mark = "A"
+let mark = "F"
 
 if mark == "A"
 {
@@ -19,4 +19,25 @@ if mark == "C"
 if mark == "D"
 {
     print("Te ha faltado poco")
+}
+
+if mark == "F"
+{
+    print("Tendrás que estudiar más")
+}
+
+switch mark {
+    case "A":
+       print("Genial! Eres un crack!")
+    case "B":
+        print("Genial! Eres un crack!")
+    case "C":
+        print("Buen trabajo! Has aprobado")
+    case "D":
+        print("Te ha faltado poco")
+    case "F":
+        print("Tendrás que estudiar más")
+    default:
+    print("La nota no es correcta")
+    
 }
