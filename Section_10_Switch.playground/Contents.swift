1a90bc752e37000821a4324783c16a6f6e99f0c8
Anartz Muxika Ledo <mugan86@gmail.com>
2017-02-08 11:07:48 +0100

    Start with playground to switch

---
+++ Section_10_Switch.playground/Contents.swift
@@ -0,0 +1,22 @@
+//Playground - Switch
+let mark = "A"
+
+if mark == "A"
+{
+    print("Genial! Eres un crack!")
+}
+
+if mark == "B"
+{
+    print("Genial! Eres un crack!")
+}
+
+if mark == "C"
+{
+    print("Buen trabajo! Has aprobado")
+}
+
+if mark == "D"
+{
+    print("Te ha faltado poco")
+}
