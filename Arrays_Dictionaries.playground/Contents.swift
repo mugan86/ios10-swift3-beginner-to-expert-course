20d9654c41c114956d4c98bdb97c3a327a7a0d0c
Anartz Muxika Ledo <mugan86@gmail.com>
2017-02-09 09:07:36 +0100

    Start with first steps and make exercise SECTION 12 - LECTURE 116

---
+++ Arrays_Dictionaries.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - Arrays / Dictionaries
+
+var shoppingList : [String] = ["Pan", "Huevos", "Leche", "Chocolate"]
+
+var luckyNumbers : [Int] = [7, 83, 49, 30]
+
+var weights : [Double] = [49.4, 40.5, 39.4]
+
+var activeItems : [Bool] = [true, false, false, false]
+
+/*
+ Sección 12, Clase 116
+ Crea un array de strings llamado favoriteFoods y rellénalo con 5 tipos de comida que te gusten.
+ A continuación crea otro array de enteros y añade los 3 primeros números de teléfono que te vengan a la mente
+ */
+
+var favoriteFoods : [String] = ["Salad", "Fish", "Spanish Omelette", "Chicken", "Pistachio"]
+
+var telephoneNumbers : [Int] = [928383838, 934383838, 938272727]
+
+//Count elements of list
+
+shoppingList.count
+
+//Add new element
+
+shoppingList.append("Chicken")
+
+shoppingList.count
+
+shoppingList.append("Coca-Cola")
+
+shoppingList.count
+
+luckyNumbers.append(23)
+
+luckyNumbers.count
+
