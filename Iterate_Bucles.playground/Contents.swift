fd182f46cd2d4fa7c2fe055fe611e8d5281291c4
Anartz Muxika Ledo <mugan86@gmail.com>
2017-02-10 11:33:51 +0100

    Finish section lecture 150 exercise

--- Iterate_Bucles.playground/Contents.swift
+++ Iterate_Bucles.playground/Contents.swift
@@ -1,5 +1,8 @@
 //: Playground - Iterate and bucles
 
+//Para usar el framework Foundation
+import Foundation
+
 //SECTION 15 LECTURE 149
 
 //De menos a más
@@ -25,4 +28,37 @@ print("***************************************")
 for var i in (1...10).reversed() //de 1 a 10 (incluido)
 {
     print("La variable i vale: \(i)")
+}
+
+/********************************************************************
+ Ejercicios de bucle for
+ Sección 15, Clase 150
+ Crea un bucle for que vaya de 1 a 100 y, en cada iteración imprima 'Voy por la iteración xx'
+ Crea un bucle for que vaya de 100 a 1 y en cada iteración imprima 'Tengo todavía xx cervezas en la despensa'
+ Crea un bucle que imprima las 10 primeras potencias de 2 (2, 4, 8, 16, 32...)
+ *****************************************************************/
+
+print("***************************************")
+
+//Crea un bucle for que vaya de 1 a 100 y, en cada iteración imprima 'Voy por la iteración xx'
+
+for var i in 1...100
+{
+    print("Voy por la iteracción: \(i)")
+
+}
+
+print("***************************************")
+//Crea un bucle for que vaya de 100 a 1 y en cada iteración imprima 'Tengo todavía xx cervezas en la despensa'
+for var i in (1...100).reversed()
+{
+    print("Tengo todavía \(i) cervezas en la despensa")
+}
+
+//Crea un bucle que imprima las 10 primeras potencias de 2 (2, 4, 8, 16, 32...)
+print("***************************************")
+print("Las 10 primeras potencias de 2 son: ")
+for var i in 1...10
+{
+    print("2^\(i)= \(pow(2,i))")
 }
