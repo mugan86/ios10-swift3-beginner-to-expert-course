0d283b198adf6a065f3f042b7e40cbdbf273687a
Anartz Muxika Ledo <mugan86@gmail.com>
2017-02-15 09:49:43 +0100

    Complete quiz

--- SECTION 13 - Nil.playground/Contents.swift
+++ SECTION 13 - Nil.playground/Contents.swift
@@ -1,5 +1,31 @@
-//: Playground - noun: a place where people can play
+//: Playground - SECTION 13 - Nil
 
-import UIKit
+var str : String
+str = "Hola"
 
-var str = "Hello, playground"
+print(str)
+
+
+var dict = [3: "Tres"]
+
+print (dict[6]) //nill <-> null
+
+//CUestionario
+
+/*1) El nil representa
+
+ - un valor aun no asignado
+ - un valor desconocido
+ 
+ 2) Los nils son útiles para
+ 
+ Los nils son útiles para
+ 
+ 
+ Poder asignar valores a posteriori
+ 
+ Destruir variables una vez dejan de ser útiles
+ 
+ Trabajar con ausencia o presencia de valores
+
+ */
